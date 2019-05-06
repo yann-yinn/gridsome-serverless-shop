6281d5db0d74019d5083c3a31624456558dc2c1a
yann <yann.boisselier@gmail.com>
2019-05-06 22:40:52 +0200

    test functons

---
+++ server/functions-src/test.js
@@ -0,0 +1,18 @@
+require("../lib/bootstrap");
+const { Shoe } = require("../lib/mongooseModels");
+
+exports.handler = function(event, context, callback) {
+  return Shoe.find({})
+    .then(r => {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(r)
+      });
+    })
+    .catch(e => {
+      callback(e, {
+        statusCode: 500,
+        body: e
+      });
+    });
+};
